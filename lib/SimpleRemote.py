f2157235a9320ba381d189f6bc1891d98857da0a
hanchi <hanchi@qianxin.com>
2020-03-29 17:55:00 +0800

    add method to run script

--- lib/SimpleRemote.py
+++ lib/SimpleRemote.py
@@ -1,6 +1,10 @@
 from robot.libraries.Remote import Remote
 from robot.libraries.Remote import XmlRpcRemoteClient
 from robot.utils import timestr_to_secs
+try:
+    import xmlrpclib
+except ImportError:  # Py3
+    import xmlrpc.client as xmlrpclib
 
 class SimpleRemote(Remote):
 
@@ -13,14 +17,26 @@ class SimpleRemote(Remote):
         self._client = SimpleClient(uri, timeout)
 
     def run_script(self, script):
-        self._client.run_script(script)
+        return self._client.run_script(script)
+
+    def transfer_file(self, local_file, target_file):
+        return self._client.transfer_file(local_file, target_file)
 
 class SimpleClient(XmlRpcRemoteClient):
 
     def run_script(self, script):
-        self._server.run_script(script)
+        return self._server.run_script(script)
+
+    def transfer_file(self, local_file, target_file):
+            with open(local_file, "rb") as handle:
+                binary_data = xmlrpclib.Binary(handle.read())
+            return self._server.save_file(target_file, binary_data)
 
 if __name__=="__main__":
     sr = SimpleRemote()
-    sr.run_script("import os")
-
+    script = '''
+import os
+print(os.path.exists("D:\\test\\1.txt"))
+    '''
+    import re
+    print(sr.run_script(re.escape(script)))
--- target/remoteserver.py
+++ target/remoteserver.py
@@ -1,15 +1,47 @@
 from robotremoteserver import RobotRemoteServer
-import os
+from robotremoteserver import StandardStreamInterceptor
+from robotremoteserver import KeywordResult
+from robotremoteserver import BINARY
+import os,sys
 
 class RemoteServer(RobotRemoteServer):
     def _register_functions(self, server):
         server.register_function(self.run_script)
+        server.register_function(self.save_file)
         RobotRemoteServer._register_functions(self, server)
 
     def run_script(self, script):
+        sr = ScriptRunner(script)
         print(script)
+        return sr.run_script()
+
+    def save_file(self, target_file, binary_data):
+        with open(target_file, "wb") as handle:
+            handle.write(binary_data.data)
+            handle.close()
+        return True
+
+class ScriptRunner():
+    def __init__(self, script):
+        self._script = script
+
+    def run_script(self):
+        result = KeywordResult()
+        with StandardStreamInterceptor() as interceptor:
+            try:
+                return_value = exec(self._script)
+            except Exception:
+                result.set_error(*sys.exc_info())
+            else:
+                try:
+                    result.set_return(return_value)
+                except Exception:
+                    result.set_error(*sys.exc_info()[:2])
+                else:
+                    result.set_status('PASS')
+        result.set_output(interceptor.output)
+        return result.data
 
 if __name__ == "__main__":
     lib = os
-    rs = RemoteServer(os, serve=False)
-    rs.serve()
+    rs = RemoteServer(os)
